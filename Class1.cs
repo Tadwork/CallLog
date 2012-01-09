11e7e6eaadefe8c064d354aee5aabbff500964b1
Tzvi Friedman <Tadwork@yahoo.com>
2012-01-09 17:16:32 -0500

    initial

---
+++ Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+namespace FPECallLog
+{
+    class CallLog
+    {
+        public DataTable GetTable() {
+
+            return;
+        }
+    }
+}
---
+++ Window1.xaml.cs
@@ -0,0 +1,46 @@
+﻿namespace FPECallLog
+{
+    #region Namespaces
+    using System;
+    using System.Diagnostics;
+    using System.Windows;
+
+    #endregion
+
+    /// <summary>
+    ///   Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        /// <summary>
+        ///   Initializes a new instance of the <see cref = "Window1" /> class.
+        /// </summary>
+        public Window1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        ///   Handles the Click event of the buttonSpawn control.
+        /// </summary>
+        /// <param name = "sender">The source of the event.</param>
+        /// <param name = "e">The <see cref = "System.Windows.RoutedEventArgs" /> instance containing the event data.</param>
+        //private void buttonSpawn_Click(object sender, RoutedEventArgs e)
+        //{
+        //    string path = GetType().Assembly.Location;
+
+        //    try
+        //    {
+        //        Process.Start(path, "those are the other application arguments");
+        //    }
+        //    catch (Exception exc)
+        //    {
+        //        MessageBox.Show(this, "An exception occurred while spawning another application" + Environment.NewLine + exc);
+        //    }
+        //}
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            CallGrid.ItemsSource = 
+        }
+    }
+}
