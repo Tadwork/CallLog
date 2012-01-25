0fe7242544d7ff43efae076bb5c2a3194ac9cb6d
Tzvi Friedman <Tadwork@yahoo.com>
2012-01-25 00:22:22 -0500

    what is wrong please commit

---
+++ CallItem.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using ServiceStack.OrmLite.Sqlite;
+using ServiceStack.DataAnnotations;
+using ServiceStack.OrmLite;
+namespace FPECallLog
+{
+    public class CallItem
+    {
+        [AutoIncrement,Index(Unique = true)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public DateTime Time { get; set; }
+        public override string ToString()
+        {
+            return Name + " " + Phone;
+        }        
+    }
+    public class Database
+    {
+        IDbCommand dbCmd;
+        #region Events
+        public event DataUpdatedHandler DataUpdated;
+        public EventArgs e = null;
+        public delegate void DataUpdatedHandler(CallItem c, EventArgs e); 
+        #endregion
+        public Database(string path)
+        {
+            //Use in-memory Sqlite DB instead
+            var dbPath = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Calls.db");
+            var dbFactory = new OrmLiteConnectionFactory(dbPath, false, SqliteOrmLiteDialectProvider.Instance);
+
+            //Non-intrusive: All extension methods hang off System.Data.* interfaces
+            IDbConnection dbConn = dbFactory.OpenDbConnection();
+            dbCmd = dbConn.CreateCommand();
+
+            ////Re-Create all table schemas:
+            //dbCmd.DropTable<CallItem>();
+            dbCmd.CreateTable<CallItem>();
+        }
+
+        #region Read
+        public IList<CallItem> TodaysCalls()
+        {
+            return dbCmd.Select<CallItem>().Where(x => x.Time.Date == DateTime.Now.Date).ToList();
+        }
+
+        public CallItem QueryCall(int id)
+        {
+            return dbCmd.Select<CallItem>().Where(x => x.Id == id).FirstOrDefault();
+        } 
+        #endregion
+        #region Create
+        public void AddCall(string name, string phone)
+        {
+
+            AddCall(new CallItem()
+            {
+                Name = name,
+                Phone = phone,
+                Time = DateTime.Now
+            });
+        }
+        public void AddCall(CallItem call)
+        {
+            dbCmd.Insert<CallItem>(call);
+            //Console.WriteLine("{0}", s.Id);
+        } 
+        #endregion
+   
+    }
+}
---
+++ Window1.xaml.cs
@@ -0,0 +1,82 @@
+﻿namespace FPECallLog
+{
+    #region Namespaces
+    using System;
+    using System.Diagnostics;
+    using System.Windows;
+    using System.Data;
+using System.ComponentModel;
+    using System.Collections.Generic;
+    using System.Windows.Data;
+    #endregion
+
+    /// <summary>
+    ///   Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        
+        /// <summary>
+        ///   Initializes a new instance of the <see cref = "Window1" /> class.
+        /// </summary>
+        public Window1()
+        {
+            InitializeComponent();
+
+            
+        }
+        /// <summary>
+        ///   Handles the Click event of the buttonSpawn control.
+        /// </summary>
+        /// <param name = "sender">The source of the event.</param>
+        /// <param name = "e">The <see cref = "System.Windows.RoutedEventArgs" /> instance containing the event data.</param>
+        //private void buttonSpawn_Click(object sender, RoutedEventArgs e)
+        //{
+        //    string path = GetType().Assembly.Location;
+
+        //    try
+        //    {
+        //        Process.Start(path, "those are the other application arguments");
+        //    }
+        //    catch (Exception exc)
+        //    {
+        //        MessageBox.Show(this, "An exception occurred while spawning another application" + Environment.NewLine + exc);
+        //    }
+        //}
+       
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {DataContext = new CallViewModel();
+        }
+
+        private void List_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            CallItem i= (CallItem)e.AddedItems[0];
+            string path = "C:\\Users\\tzvi\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe";
+            string args = "\"docs.google.com/a/friedmanpe.com/spreadsheet/viewform?pli=1&hl=en_US&formkey=dGhvdERPU0hjREV5S2ZSb0VIenVXN0E6MQ&entry_0=" + i.Name + "&entry_5=" +i.Phone +"&entry_8=Tzvi\"";
+            Process.Start(path,args);
+        }
+       
+
+       
+    }
+    public class CallViewModel
+        {   private Database  db;
+            private ICollectionView _callsView;
+
+            public ICollectionView Calls
+            {
+                get { return _callsView; }
+            }
+
+            public CallViewModel()
+            {
+                UpdateCalls();
+            }
+            public void UpdateCalls(){
+                var dbPath = System.IO.Path.Combine (Environment.GetFolderPath (Environment.SpecialFolder.MyDocuments), "Calls.db");
+			    db = new Database (dbPath);
+                IList<CallItem> calls = db.TodaysCalls();
+                _callsView = CollectionViewSource.GetDefaultView(calls);
+            }
+        }
+}
